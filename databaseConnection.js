3a646a5de9a00058e16434976186bb66d16561b6
Ziad Malik <zmalik4@my.bcit.ca>
2026-05-11 21:39:45 -0700

    final changes

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,14 +1,10 @@
-const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
-const mongodb_host     = process.env.MONGODB_HOST;
-const mongodb_user     = process.env.MONGODB_USER;
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
 
-const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/?retryWrites=true`;
-
-let database = new MongoClient(atlasURI);
-
-// Explicitly connect so queries don't run on an unready client
-database.connect();
-
-module.exports = { database };
+const MongoClient = require("mongodb").MongoClient;
+const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/`;
+var database = new MongoClient(atlasURI, {});
+module.exports = {database};
